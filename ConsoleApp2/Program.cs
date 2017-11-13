591bf4f1d8d5ed58948addb78b54b25153bb9648
afm <afm@crossworkers.com>
2017-11-14 01:19:04 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graphviz4Net.Dot.AntlrParser;
+using Graphviz4Net.Graphs;
+using Stateless;
+using Stateless.Graph;
+using Stateless.Visualization;
+
+namespace ConsoleApp2
+{
+    enum UserStates{Empty,Active,New,LoggedIn, LoggedOut }
+    enum Trigger { RegisterAccount, VerifyEmail, LogIn,LogOut,Deactivate }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userRegistrationMachine= new StateMachine<UserStates, Trigger>(UserStates.Empty);
+
+            userRegistrationMachine.Configure(UserStates.Empty)
+                .Permit(Trigger.RegisterAccount, UserStates.New);
+
+            userRegistrationMachine.Configure(UserStates.New)
+                .Permit(Trigger.VerifyEmail, UserStates.Active);
+
+            userRegistrationMachine.Configure(UserStates.Active)
+                .Permit(Trigger.LogIn, UserStates.LoggedIn)
+                .Permit(Trigger.Deactivate, UserStates.New);
+
+            userRegistrationMachine.Configure(UserStates.LoggedIn)
+                .SubstateOf(UserStates.Active)
+                .Permit(Trigger.LogOut, UserStates.LoggedOut);
+
+            userRegistrationMachine.Configure(UserStates.LoggedOut)
+                .SubstateOf(UserStates.Active)
+                .Permit(Trigger.LogIn, UserStates.LoggedIn);
+
+            userRegistrationMachine.Activate();
+            using (var sw = new StreamWriter("graph1.dot"))
+            {
+               sw.Write( userRegistrationMachine.ToGraph());
+                sw.Flush();
+            }
+            return;
+            var graph=new Graph<UserStates>();
+
+            foreach (var stateInfo in userRegistrationMachine.GetInfo().States)
+            {
+                graph.AddVertex((UserStates)stateInfo.UnderlyingState);
+
+            }
+            foreach (var stateInfo in userRegistrationMachine.GetInfo().States)
+            {
+                foreach (var stateInfoTransition in stateInfo.FixedTransitions)
+                {
+                    var sourceState = (UserStates)stateInfo.UnderlyingState;
+                    var destinationState = (UserStates)stateInfoTransition.DestinationState.UnderlyingState;
+                    graph.AddEdge(new Edge<UserStates>(sourceState, destinationState){Label = stateInfoTransition.Trigger.ToString()});
+
+                }
+            }
+            graph.Ratio = 100;
+            var value = graph.ToString();
+            using (var sw=new StreamWriter("graph.dot"))
+            {
+                value= value.Replace(":", "{");
+                value = value.Replace("graph", "digraph");
+                value = value.Replace("--", "->");
+                value = value + "}";
+                sw.Write(value);
+                sw.Flush();
+            }
+            Console.WriteLine(value);
+            //Console.WriteLine(userRegistrationMachine.GetInfo().States.FirstOrDefault().Transitions.FirstOrDefault().Trigger.ToString());
+            Console.WriteLine(userRegistrationMachine.CanFire(Trigger.LogIn));
+        }
+    }
+}
---
+++ Stateless.Visualization/StateMachineExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stateless.Visualization
+{
+    public static class StateMachineExtensions
+    {
+        public static string ToGraph<S,T>(this StateMachine<S,T> stateMachine)
+        {
+            StringBuilder result=new StringBuilder();
+            result.AppendLine("digraph");
+            result.AppendLine("\t {");
+            foreach (var stateInfo in stateMachine.GetInfo().States)
+            {
+                result.AppendLine($"\t\t {stateInfo.UnderlyingState}");
+            }
+
+            foreach (var stateInfo in stateMachine.GetInfo().States)
+            {
+                foreach (var stateInfoTransition in stateInfo.FixedTransitions)
+                {
+                    result.AppendLine(
+                        $"\t\t {stateInfo.UnderlyingState} -> {stateInfoTransition.DestinationState.UnderlyingState} [label=\"{stateInfoTransition.Trigger}\" ]");
+                }
+            }
+            result.AppendLine("\t }");
+
+            return result.ToString();
+
+        }
+    }
+}
